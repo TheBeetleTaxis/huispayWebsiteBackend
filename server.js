83a1deee87c125d7572e897a1e5085196f22b5ed
TheBeetleTaxis <beetletaxis@gmail.com>
2023-07-04 22:59:33 +0100

    cors error fixed

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
@@ -17,6 +18,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use(helmet());
 
 app.use(cookieParser());
 
