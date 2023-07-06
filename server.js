36d4715a58fc2fb22defc55d806af19a68250280
TheBeetleTaxis <beetletaxis@gmail.com>
2023-07-06 10:01:59 +0100

    addition

--- server.js
+++ server.js
@@ -34,10 +34,10 @@ app.use('/api/training', trainingRoutes);
 
 if (process.env.NODE_ENV === 'production') {
 	const __dirname = path.resolve();
-	app.use(express.static(path.join(__dirname, '/frontend/dist')));
+	app.use(express.static(path.join(__dirname, 'server.js')));
 
 	app.get('*', (req, res) =>
-		res.sendFile(path.resolve(__dirname, 'huispayWesiteBackend', 'dist', 'server.js'))
+		res.sendFile(path.resolve(__dirname, 'server.js'))
 	);
 } else {
 	app.get('/', (req, res) => {
