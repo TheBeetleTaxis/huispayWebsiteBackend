dfc97ee2fc2fb660e5bdd15bd10cd79c9f436156
TheBeetleTaxis <beetletaxis@gmail.com>
2023-09-21 14:18:01 +0100

    fixed cors errors

--- server.js
+++ server.js
@@ -25,7 +25,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],
+    credentials: false,
+  })
+);
 app.use(helmet());
 
 app.use(cookieParser());
