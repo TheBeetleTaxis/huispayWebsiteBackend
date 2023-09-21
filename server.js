c4d5fd7471617db24b9870b4394f95000db7fa60
TheBeetleTaxis <beetletaxis@gmail.com>
2023-09-21 15:56:04 +0100

    fixed third cors errors

--- server.js
+++ server.js
@@ -25,13 +25,34 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use(
-  cors({
+app.use(cors({
     origin: "*",
     methods: ["GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],
-    credentials: true,
-  })
-);
+    credentials: false,
+    
+}));
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.header('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  // res.header('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+
 app.use(helmet());
 
 app.use(cookieParser());
