3f86eed260ce864a56f029dc814d811934211067
TheBeetleTaxis <beetletaxis@gmail.com>
2023-09-21 14:31:30 +0100

    fixed redis errors

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
-import redis from 'redis';
 dotenv.config();
 import connectDB from './config/db.js';
 import cors from 'cors';
